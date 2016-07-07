ed6f1a0ed6f239e521bda77b0d576ac1628d6b3c
root <root@fresh.ubuntu.com>
2016-07-07 23:55:58 +0530

    modified if condition

---
+++ centos_New_system.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "This is a shell script written to configure your system automatically and to run this script make sure you are a root user"
+if [ "$(whoami)" != "root" ] ; then
+  echo "Switch to root user you DUMMY!!!!"
+  exit 1
+fi
+echo "Hello , This is the Menu. Select which one you want to do"
+echo '1. Change Hostname \n  2. Set Static IP \n  3. Install SSH Server'
